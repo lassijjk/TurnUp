fb38f63bcf77745c168ad1e9aa57970d81528efb
finye <finnantiku@gmail.com>
2023-09-13 23:11:46 +0300

    use digi api in its basic form

---
+++ client/src/components/CommutingStops.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { VITE_DIGI_TRANSIT_API_KEY } from '../constants'
+
+const CommutingStops: React.FC = () => {
+  const [data, setData] = useState<any>(null)
+  const [error, setError] = useState<string | null>(null)
+
+  const handleGraphQLRequest = async () => {
+    try {
+      const response = await axios.post(
+        `https://api.digitransit.fi/routing/v1/routers/waltti/index/graphql?digitransit-subscription-key=${VITE_DIGI_TRANSIT_API_KEY}`,
+        {
+          query: `
+            query {
+                plan(
+                  from: {lat: 61.4941, lon: 23.7792}
+                  to: {lat: 61.5038, lon: 23.8088}
+                  numItineraries: 1
+                ) {
+                  itineraries {
+                    legs {
+                      startTime
+                      endTime
+                      mode
+                      duration
+                      realTime
+                      distance
+                      transitLeg
+                    }
+                  }
+                }
+              }
+          `,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+
+      setData(response.data.data)
+      setError(null)
+    } catch (err) {
+      setError(err + ' An error occurred while fetching data.')
+      setData(null)
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={handleGraphQLRequest}>Let's Go</button>
+      {error && <p>{error}</p>}
+      {data && (
+        <pre>
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      )}
+    </div>
+  )
+}
+
+export default CommutingStops
