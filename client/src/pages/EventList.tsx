914d044c74656b7780e59f823e1b0b3f39e52403
Anisul Mahmud <first.last@tuni.fi>
2023-09-18 02:45:35 +0300

    updated eventlist

--- client/src/pages/EventList.tsx
+++ client/src/pages/EventList.tsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import ReactPaginate from 'react-paginate'
 
 interface Location {
   address: string
@@ -28,15 +29,20 @@ interface Event {
   end_time: string
   locations: Location[]
   dates: DateInfo[]
+  // Add other fields you need here
 }
 
 const EventList: React.FC = () => {
   const [events, setEvents] = useState<Event[]>([])
+  const [currentPage, setCurrentPage] = useState(0)
+  const eventsPerPage = 10 // Set the number of events to display per page
 
   useEffect(() => {
+    // Fetch data from the API
     axios
       .get('https://api.visittampere.com/api/v1/eventztoday/event/all/?lang=en')
       .then((response) => {
+        // Extract relevant fields from the API response
         setEvents(response.data)
       })
       .catch((error) => {
@@ -44,6 +50,7 @@ const EventList: React.FC = () => {
       })
   }, [])
 
+  // Function to find the latest date within an event's dates array
   const findLatestDate = (dates: DateInfo[]) => {
     if (dates.length === 0) {
       return null
@@ -61,37 +68,82 @@ const EventList: React.FC = () => {
     )
   }
 
+  // Function to format date and time
+  const formatDate = (datetime: string) => {
+    const date = new Date(datetime)
+    return date.toLocaleDateString() // You can customize the format here
+  }
+
+  const formatTime = (datetime: string) => {
+    const date = new Date(datetime)
+    return date.toLocaleTimeString() // You can customize the format here
+  }
+
+  // Filter events happening on today's date
+  const today = new Date().toISOString().split('T')[0] // Get today's date in "YYYY-MM-DD" format
+  const todayEvents = events.filter((event) => {
+    const latestDate = findLatestDate(event.dates)
+    return latestDate && latestDate.start.startsWith(today)
+  })
+
+  // Paginate events
+  const offset = currentPage * eventsPerPage
+  const paginatedEvents = todayEvents.slice(offset, offset + eventsPerPage)
+
   return (
     <div>
       <h2>Events Today</h2>
-      <table className="event-table">
-        <thead>
-          <tr>
-            <th className="align-left">Serial Number</th>
-            <th className="align-left">Name</th>
-            <th className="align-right">Start Time</th>
-            <th className="align-right">End Time</th>
-            <th className="align-right">Latest Start Date</th>
-            <th className="align-right">Latest End Date</th>
-            <th className="align-right">Address</th>
-          </tr>
-        </thead>
-        <tbody>
-          {events.map((event, index) => (
-            <tr key={index + 1} className="table-row">
-              <td className="align-left">{index + 1}</td>
-              <td className="align-left">{event.name}</td>
-              <td className="align-right">{event.start_time}</td>
-              <td className="align-right">{event.end_time}</td>
-              <td className="align-right">{findLatestDate(event.dates)?.start || 'N/A'}</td>
-              <td className="align-right">{findLatestDate(event.dates)?.end || 'N/A'}</td>
-              <td className="align-right">{event.locations[0]?.address || 'N/A'}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="gap"></div>
+      <div className="gap2"></div>
+      <div className="gap3"></div> {/* Add a gap */}
+      <div className="event-card-container">
+        {paginatedEvents.map((event, index) => (
+          <div key={index} className="event-card">
+            <div className="event-header">
+              <div className="event-title">{event.name}</div>
+            </div>
+            <div className="event-details">
+              <div className="event-datetime">
+                <div className="event-start">
+                  <div className="label">Start Date:</div>
+                  <div className="value">{formatDate(event.start_time)}</div>
+                </div>
+                <div className="event-start">
+                  <div className="label">Start Time:</div>
+                  <div className="value">{formatTime(event.start_time)}</div>
+                </div>
+              </div>
+              <div className="event-datetime">
+                <div className="event-end">
+                  <div className="label">End Date:</div>
+                  <div className="value">{formatDate(event.end_time)}</div>
+                </div>
+                <div className="event-end">
+                  <div className="label">End Time:</div>
+                  <div className="value">{formatTime(event.end_time)}</div>
+                </div>
+              </div>
+              <div className="event-address">
+                <div className="label">Address:</div>
+                <div className="value">{event.locations[0]?.address || 'N/A'}</div>
+              </div>
+              {/* Add additional details here */}
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* Pagination */}
+      <div className="pagination-container">
+        <ReactPaginate
+          pageCount={Math.ceil(todayEvents.length / eventsPerPage)}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={2}
+          onPageChange={(selectedPage) => setCurrentPage(selectedPage.selected)}
+          containerClassName={'pagination'}
+          activeClassName={'active'}
+        />
+      </div>
     </div>
   )
 }
-
 export default EventList
