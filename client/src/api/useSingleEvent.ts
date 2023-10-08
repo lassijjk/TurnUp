c2b3abbbfad3e4c5aa5d4ba031976b9e7d1630e7
finye <finnantiku@gmail.com>
2023-10-08 19:46:32 +0300

    rename hooks and change them to .ts

--- client/src/api/useSingleEvent.ts
+++ client/src/api/useSingleEvent.ts
@@ -1,12 +1,11 @@
 import { rememberQuery } from './CacheManager.tsx'
 import { QueryId } from '../types/api.ts'
-import useTodaysEvents from './UseTodaysEvents.tsx'
 import { useStore } from '../stores/settingStore.tsx'
 
-const UseSingleEvent = (id: string) => {
+const useSingleEvent = (id: string) => {
   const { language } = useStore()
   const events = rememberQuery(QueryId.TODAYS_EVENTS, language)
   return events ? events[id] : null
 }
 
-export default UseSingleEvent
+export default useSingleEvent
