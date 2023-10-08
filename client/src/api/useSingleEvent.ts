56fbfedfd86272449415670a1c3033a3fb8ccbfa
finye <finnantiku@gmail.com>
2023-10-08 22:09:45 +0300

    update state on language change

--- client/src/api/useSingleEvent.ts
+++ client/src/api/useSingleEvent.ts
@@ -1,10 +1,12 @@
 import { rememberQuery } from './CacheManager.tsx'
 import { QueryId } from '../types/api.ts'
 import { useStore } from '../stores/settingStore.tsx'
+import { useMemo } from 'react'
 
 const useSingleEvent = (id: string) => {
   const { language } = useStore()
-  const events = rememberQuery(QueryId.TODAYS_EVENTS, language)
+  const events = useMemo(() =>  rememberQuery(QueryId.TODAYS_EVENTS, language), [language])
+  
   return events ? events[id] : null
 }
 
